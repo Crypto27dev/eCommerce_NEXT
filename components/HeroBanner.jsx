c5ee2572f5e22b6ec06c9280bf0e30b9ea1e1cd3
Alvi Hasan <livealvi@gmail.com>
2022-05-24 02:36:45 +0600

    add: HeroBanner - text

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+        <div>
+          <Link href="/product/ID">
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
