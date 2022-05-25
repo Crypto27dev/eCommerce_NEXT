976592cbca12ee4abbb078859ef1a0ab49440407
Alvi Hasan <livealvi@gmail.com>
2022-05-25 17:39:50 +0600

    add: hero banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
 import React from "react";
+import { urlForm } from "../lib/client";
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src="" alt="headphones" className="hero-banner-image" />
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img
+          src={urlForm(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
         <div>
-          <Link href="/product/ID">
-            <button type="button">BUTTON TEXT</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+import { client } from "../lib/client";
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speaker of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   );
 };
 
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
 export default Home;
