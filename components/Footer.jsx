a7cbe153ba4d3f271acbbc1a903a36c72b68edbc
Alvi Hasan <livealvi@gmail.com>
2022-05-25 19:00:48 +0600

    add: footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2022 Headphone Shop All right reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
