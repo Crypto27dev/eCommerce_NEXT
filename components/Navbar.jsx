75c829009c91c8acaff97e2a43d995b04ee6352c
Alvi Hasan <livealvi@gmail.com>
2022-05-25 18:42:16 +0600

    add: navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Online Headphones Shop</Link>
+      </p>
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 };
 
 export default navbar;
