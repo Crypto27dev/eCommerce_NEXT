295949d3498a007822e6fb415cebb2ae182871ef
Alvi Hasan <livealvi@gmail.com>
2022-05-25 18:27:48 +0600

    add: footer banner

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const Home = ({ products, bannerData }) => {
           <Product key={product._id} product={product} />
         ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
 };
