9203df2b35b15ba91557de49b7f67b614abc8519
Alvi Hasan <livealvi@gmail.com>
2022-05-25 18:37:37 +0600

    add: layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Headphones, Earphones Online Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
