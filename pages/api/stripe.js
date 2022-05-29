15dd869b313c7e79873d47c857419355b7958834
Alvi Hasan <livealvi@gmail.com>
2022-05-30 03:21:33 +0600

    fix: success page

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -39,8 +39,8 @@ export default async function handler(req, res) {
             quantity: item.quantity,
           };
         }),
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
       };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
