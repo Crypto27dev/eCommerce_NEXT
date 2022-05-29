a30751e68e4d5ca752bb2e529bfb425f3d12dc08
Alvi Hasan <livealvi@gmail.com>
2022-05-30 02:53:24 +0600

    add: confetti

--- context/StateContext.js
+++ context/StateContext.js
@@ -104,6 +104,9 @@ export const StateContext = ({ children }) => {
         onAdd,
         toggleCartItemQuantity,
         onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities,
       }}
     >
       {children}
---
+++ pages/success.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+import { runFireworks } from "../lib/utils";
+import { useStateContext } from "../context/StateContext";
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    runFireworks();
+  }, []);
+
+  const [order, setOrder] = useState(null);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:order@example.com">
+            order@example.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" className="btn" width="300px">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
